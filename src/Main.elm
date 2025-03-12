270c318e6ecaf0ee0ef88daaea44c332315ef207
purewhite404 <jr2hao.23soda@gmail.com>
2025-03-12 23:01:12 +0900

    show exposure in exif

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,9 @@ import Html exposing (Html, button, text, img, div)
 import Html.Attributes exposing (id, src, width, class)
 import Html.Events exposing (onClick)
 import Task
+import Json.Decode as Decode exposing (Decoder, int, float, string)
+import Json.Decode.Pipeline exposing (required, optional)
+import Json.Decode as Decode
 
 
 
@@ -29,7 +32,7 @@ main =
 
 
 port analyzeExif : String -> Cmd msg
-port getExif : (Int -> msg) -> Sub msg
+port getExif : (String -> msg) -> Sub msg
 
 
 
@@ -38,13 +41,51 @@ port getExif : (Int -> msg) -> Sub msg
 
 type alias Model =
   { image : Maybe String
-  , exif : Maybe Int
+  , exif : Maybe EXIF
   }
+
+type alias EXIF =
+  { make : String
+  , model : String
+  , iso : Int
+  , fNumber : Float
+  , exposureTime : Float
+  , focalLength : Float
+  , exposureProgram : String
+  , date : String
+  , lens : String
+  }
+
+exifDecoder : Decoder EXIF
+exifDecoder =
+  Decode.succeed EXIF
+    |> required "Make" string
+    |> required "Model" string
+    |> required "ISOSpeedRatings" int
+    |> required "FNumber" float
+    |> required "ExposureTime" float
+    |> required "FocalLength" float
+    |> required "ExposureProgram" string
+    |> required "DateTime" string
+    |> optional "undefined" string ""
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model Nothing Nothing, Cmd.none )
+  ( Model Nothing Nothing,  Cmd.none )
+
+initialEXIF : EXIF
+initialEXIF =
+  { make = ""
+  , model = ""
+  , iso = 0
+  , fNumber = 0
+  , exposureTime = 0
+  , focalLength = 0
+  , exposureProgram = ""
+  , date = ""
+  , lens = ""
+  }
 
 
 
@@ -55,7 +96,7 @@ type Msg
   = ImageRequested
   | ImageSelected File
   | ImageLoaded String
-  | GetExif Int
+  | GetExif String
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -78,9 +119,16 @@ update msg model =
       , analyzeExif content
       )
 
-    GetExif exif ->
-      ( {model | exif = Just exif }
-      , Cmd.none )
+    GetExif exifjson ->
+      case Decode.decodeString exifDecoder exifjson of
+        Ok exif ->
+          ( { model | exif = Just exif }
+          , Cmd.none )
+        Err error ->
+          let
+            _ = Debug.log <| Decode.errorToString error
+          in
+          ( model, Cmd.none)
 
 
 
@@ -90,19 +138,57 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  case model.image of
-    Nothing ->
-      button [ onClick ImageRequested ] [ text "Upload image" ]
-
-    Just content ->
+  case (model.image, model.exif) of
+    (Just i, Just e) ->
       div []
-        [ img [ id "picture", src content, width 400 ] []
+        [ img [ id "picture", src i, width 800 ] []
         , div [ class "description" ]
-          [ div [] [ text <| String.concat ["ISO ", String.fromInt <| Maybe.withDefault 200 model.exif ] ]
-          , div [] [ text "OLYMPUS" ]
+          [ div [ class "exifcell left" ]
+            [ div [ class "exifelement" ] [text <| e.make ]
+            ]
+          , div [ class "exifcell right" ]
+            [ div [ class "exifelement" ] [ text <| String.fromFloat e.focalLength ++ "mm" ]
+            , div [ class "exifelement" ] [ text <| "F" ++ String.fromFloat e.fNumber ]
+            , div [ class "exifelement" ] [ text <| shutterspeed e.exposureTime ]
+            , div [ class "exifelement" ] [ text <| "ISO" ++ String.fromInt e.iso ]
+            ]
+          , div [ class "exifcell left" ]
+            [ div [ class "exifelement" ] [ text <| e.model ++ "/" ]
+            , div [ class "exifelement" ] [ text <| e.lens ]
+            ]
+          , div [ class "exifcell right" ]
+            [ div [ class "exifelement" ] [ text <| e.exposureProgram ]
+            , div [ class "exifelement" ] [ text <| showdate e.date ]
+            ]
           ]
+        , button [ onClick ImageRequested ] [ text "Upload image" ]
         ]
 
+    (Just i, Nothing) ->
+      div []
+        [ img [ id "picture", src i, width 800 ] []
+        , div [ class "description" ] []
+        , button [ onClick ImageRequested ] [ text "Upload image" ]
+        ]
+
+    _ ->
+      button [ onClick ImageRequested ] [ text "Upload image" ]
+
+shutterspeed : Float -> String
+shutterspeed exposureTime =
+  if exposureTime < 1
+  then "1/" ++ (String.fromInt <| round (1/exposureTime)) ++ "s"
+  else String.fromFloat exposureTime ++ "s"
+
+showdate : String -> String
+showdate d =
+  let
+    date = d 
+      |> String.left 10
+      |> String.replace ":" "/"
+    time = String.right 9 d
+  in
+  date ++ time
 
 -- SUBSCRIPTIONS
 
